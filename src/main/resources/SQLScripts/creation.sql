39a3e5aad4d25c78df0d287e65eadae0af3e0a42
Apostu Daniel <apostudanbs@gmail.com>
2022-05-29 20:40:20 +0300

    Changed creation.sql to postgress

--- src/main/resources/SQLScripts/creation.sql
+++ src/main/resources/SQLScripts/creation.sql
@@ -1,110 +1,50 @@
 CREATE TABLE station(
-    id NUMBER,
-    name VARCHAR2(50) NOT NULL,
-    coordinates_x NUMBER(8,2),
-    coordinates_y NUMBER(8,2),
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50) NOT NULL,
+    coordinates_x DECIMAL(8,2),
+    coordinates_y DECIMAL(8,2),
     total_bike_capacity int,
     bikes_stationed int,
     optimal_bike_count int,
-    total_cars int,
-    cars_stationed int,
+    is_depo boolean,
     PRIMARY KEY(id)
 );
 
-CREATE SEQUENCE station_seq START WITH 1;
-
-CREATE OR REPLACE TRIGGER station_inc
-BEFORE INSERT ON station
-FOR EACH ROW
-BEGIN
-  SELECT station_seq.NEXTVAL
-  INTO   :new.id
-  FROM   dual;
-END;
-/
-
 DROP TABLE station;
-DROP SEQUENCE station_seq;
-DROP TRIGGER station_inc;
 
 CREATE TABLE route(
-    id NUMBER PRIMARY KEY,
-    station_src NUMBER,
-    station_dest NUMBER,
-    length NUMBER,
+    id INT GENERATED ALWAYS AS IDENTITY,
+    station_src INT,
+    station_dest INT,
+    length DECIMAL(15,3),
     CONSTRAINT fk_src FOREIGN KEY (station_src) REFERENCES station(id) ON DELETE CASCADE,
-    CONSTRAINT fk_dest FOREIGN KEY (station_dest) REFERENCES station(id) ON DELETE CASCADE
+    CONSTRAINT fk_dest FOREIGN KEY (station_dest) REFERENCES station(id) ON DELETE CASCADE,
+    PRIMARY KEY(id)
 );
 
-CREATE SEQUENCE route_seq START WITH 1;
-
-CREATE OR REPLACE TRIGGER route_inc
-BEFORE INSERT ON route
-FOR EACH ROW
-BEGIN
-  SELECT route_seq.NEXTVAL
-  INTO   :new.id
-  FROM   dual;
-END;
-/
 
 DROP TABLE route;
-DROP SEQUENCE route_seq;
-DROP TRIGGER route_inc;
-
 
 CREATE TABLE car(
-    id NUMBER PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     total_capacity int,
     current_load int,
-    current_route NUMBER,
-    CONSTRAINT fk_route FOREIGN KEY (current_route) REFERENCES route(id) ON DELETE SET NULL
+    current_route INT,
+    CONSTRAINT fk_route FOREIGN KEY (current_route) REFERENCES route(id) ON DELETE SET NULL,
+    PRIMARY KEY(id)
 );
-
-CREATE SEQUENCE car_seq START WITH 1;
-
-CREATE OR REPLACE TRIGGER car_inc
-BEFORE INSERT ON car
-FOR EACH ROW
-BEGIN
-  SELECT car_seq.NEXTVAL
-  INTO   :new.id
-  FROM   dual;
-END;
-/
 
 DROP TABLE car;
-DROP SEQUENCE car_seq;
-DROP TRIGGER car_inc;
-
 
 CREATE TABLE bike(
-    id NUMBER PRIMARY KEY,
-    in_maintenance NUMBER(1),
-    station_id NUMBER,
-    route_id NUMBER,
-    time_used_seconds NUMBER,
+    id INT GENERATED ALWAYS AS IDENTITY,
+    in_maintenance boolean,
+    station_id INT,
+    route_id INT,
+    time_used_seconds INT,
     CONSTRAINT fk_bike_route FOREIGN KEY (route_id) REFERENCES route(id) ON DELETE SET NULL,
-    CONSTRAINT fk_bike_station FOREIGN KEY (station_id) REFERENCES station(id) ON DELETE SET NULL
+    CONSTRAINT fk_bike_station FOREIGN KEY (station_id) REFERENCES station(id) ON DELETE SET NULL,
+    PRIMARY KEY(id)
 );
 
-CREATE SEQUENCE bike_seq START WITH 1;
-
-CREATE OR REPLACE TRIGGER bike_inc
-BEFORE INSERT ON bike
-FOR EACH ROW
-BEGIN
-  SELECT bike_seq.NEXTVAL
-  INTO   :new.id
-  FROM   dual;
-END;
-/
-
-DROP TABLE bike;
-DROP SEQUENCE bike_seq;
-DROP TRIGGER bike_inc;
-
-CREATE OR REPLACE PROCEDURE TESTP(v_number OUT INT) AS
-BEGIN
-    SELECT 1 INTO v_number FROM DUAL;
-END TESTP;
+drop table bike;
